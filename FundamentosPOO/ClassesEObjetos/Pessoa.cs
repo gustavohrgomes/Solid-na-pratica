68ec780831491cb2db755c24a7a0f0601eb1fa37
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-05-31 20:40:34 -0300

    Classes, objetos, construtores e sobrecarga

---
+++ FundamentosPOO/ClassesEObjetos/Pessoa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesEObjetos
+{
+    class Pessoa
+    {
+        public string nome;
+        public int idade;
+        public string genero;
+
+        public Pessoa()
+        {
+        }
+
+        public Pessoa(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public Pessoa(string nome, int idade, string genero)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.genero = genero;
+        }
+
+        public void Identificar()
+        {
+            Console.WriteLine($"Olá, sou o {nome} tenho {idade} anos, sou do sexo {genero}");
+        }
+    }
+}
