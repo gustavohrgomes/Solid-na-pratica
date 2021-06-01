2500b2958f1d138121896af30e32ad334629bb45
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-06-01 08:00:45 -0300

    Argumentos Nomeados

---
+++ FundamentosPOO/ArgumentosNomeados/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ArgumentosNomeados
+{
+    class Program
+    {
+        static void Enviar(string destino, string titulo, string assunto)
+        {
+            Console.WriteLine($"{destino} - {titulo} - {assunto}");
+        }
+        static void Main(string[] args)
+        {
+            Enviar(destino: "email_teste@gmail.com", assunto: "teste", titulo: "argumentos");
+            Enviar(destino: "email_teste@gmail.com", titulo: "argumentos", assunto: "teste");
+            Enviar(titulo: "argumentos", assunto: "teste", destino: "email_teste@gmail.com");
+            Enviar(assunto: "teste", titulo: "argumentos", destino: "email_teste@gmail.com");
+        }
+    }
+}
