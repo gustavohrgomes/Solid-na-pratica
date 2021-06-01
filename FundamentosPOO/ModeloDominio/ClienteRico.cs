e893c48b14f7a27ee3e4aeeecd6dc02e9cf26b8c
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-06-01 07:30:41 -0300

    Exemplo de modelo de dominio anemico e modelo de dominio rico

---
+++ FundamentosPOO/ModeloDominio/ClienteRico.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModeloDominio
+{
+    class ClienteRico
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Endereco { get; private set; }
+
+        public ClienteRico(int id, string nome, string endereco)
+        {
+            // Validação mais robusta, não é muito recomendado
+
+            //if (id <= 0)
+            //    throw new InvalidOperationException("Id não pode ser menor ou igual a 0");
+
+            //if (string.IsNullOrEmpty(nome) || string.IsNullOrEmpty(endereco))
+            //    throw new InvalidOperationException();
+
+            // Validação abaixo também não algo a ser seguido, o ideal seria implementa o
+            // Notification Pattern, criado por Martin Fowler
+
+            DomainExceptionValidation.When(id <= 0, "Id não pode ser menor ou igual a 0.");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(nome), "O campo nome não pode estar vazio.");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(endereco), "O campo endereco não pode estar vazio");
+
+            Id = id;
+            Nome = nome;
+            Endereco = endereco;
+        }
+    }
+}
---
+++ FundamentosPOO/ModeloDominio/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ModeloDominio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClienteRico cliente = new ClienteRico(-1, "", null);
+
+            Console.WriteLine($"{cliente.Id} {cliente.Nome} {cliente.Endereco}");
+        }
+
+        private static void ClienteAnemico()
+        {
+            ClienteAnemico cliente = new ClienteAnemico();
+            cliente.Id = -1;
+            cliente.Nome = "";
+            cliente.Endereco = null;
+
+            Console.WriteLine($"{cliente.Id} {cliente.Nome} {cliente.Endereco}");
+        }
+    }
+}
