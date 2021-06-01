dd051925ce3810d374aee18b437399ead76520e0
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-06-01 06:54:50 -0300

    Propiedades e princípios de encapsulamento

---
+++ FundamentosPOO/Propriedades/Pessoa.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Propriedades
+{
+    public class Pessoa
+    {
+        private string nome;
+        private string genero;
+        private int idade;
+
+        public Pessoa(string nome)
+        {
+            this.nome = nome;
+        }
+        public string Nome { get { return nome; } }
+
+        public string Genero
+        {
+            set
+            {
+                genero = value;
+                Console.WriteLine(genero);
+            }
+        }
+
+        public int Idade
+        {
+            set
+            {
+                if(value < 18)
+                {
+                    System.Console.WriteLine("Idade tem que ser maior que 18");
+                }
+                else
+                {
+                    this.idade = value;
+                }
+            }
+            get { return idade; }
+        }
+    }
+}
