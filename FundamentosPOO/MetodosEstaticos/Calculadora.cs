f6b14a54fcd06dce321a718be22d9bd1312a22ba
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-06-02 07:15:58 -0300

    Metodos Estaticos

---
+++ FundamentosPOO/MetodosEstaticos/Calculadora.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetodosEstaticos
+{
+    class Calculadora
+    {
+        public static int Somar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int Subtrair(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+    }
+}
---
+++ FundamentosPOO/MetodosEstaticos/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MetodosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adição: " + Calculadora.Somar(38, 24));
+            Console.WriteLine("Subtração: " + Calculadora.Subtrair(38, 24));
+        }
+    }
+}
