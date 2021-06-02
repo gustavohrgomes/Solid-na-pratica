4519d7c89070a7c674e7809ba6b5d1a79f1d4b92
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-06-02 07:34:38 -0300

    Metodos de extensao

---
+++ FundamentosPOO/MetodosExtensao/Extensions/MetodosExtensao.cs
@@ -0,0 +1,17 @@
+﻿namespace MetodosExtensao.Extensions
+{
+    public static class MetodosExtensao
+    {   
+        public static string CaixaAltaPrimeiraLetra(this string valor)
+        {
+            if (valor.Length > 0)
+            {
+                char[] array = valor.ToCharArray();
+                array[0] = char.ToUpper(array[0]);
+                return new string(array);
+            }
+
+            return valor;
+        }
+    }
+}
---
+++ FundamentosPOO/MetodosExtensao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MetodosExtensao.Extensions;
+
+namespace MetodosExtensao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto1 = "curso";
+            var texto2 = "testando 123";
+
+            texto1 = texto1.CaixaAltaPrimeiraLetra();
+            texto2 = texto2.CaixaAltaPrimeiraLetra();
+
+            Console.WriteLine(texto1);
+            Console.WriteLine(texto2);
+        }
+    }
+}
