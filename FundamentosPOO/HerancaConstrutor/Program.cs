c0c4640c9f3a89a6a2b4696e59a4039f953d41b0
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-06-03 07:34:27 -0300

    Herança: Construtores

---
+++ FundamentosPOO/HerancaConstrutor/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace HerancaConstrutor
+{
+    class Veiculo
+    {
+        private string _numeroChassis;
+
+
+        public Veiculo()
+        {
+            Console.WriteLine("Inicializando veiculo.");
+        }
+        public Veiculo(string numeroChassis)
+        {
+            _numeroChassis = numeroChassis;
+            Console.WriteLine("Inicializando veiculo: " + numeroChassis);
+        }
+
+        public virtual void Teste()
+        {
+            Console.WriteLine("Acessei teste");
+        }
+    }
+
+    class Carro : Veiculo 
+    {
+        public Carro()
+        {
+            Console.WriteLine("Inicializando carro.");
+        }
+
+        public Carro(string numeroChassis) : base(numeroChassis)
+        {
+            Console.WriteLine("Inicializando carro: " + numeroChassis);
+        }
+
+        public override void Teste()
+        {
+            base.Teste();
+            Console.WriteLine("Sobrescrevi teste");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var carro = new Carro("123456");
+            carro.Teste();
+        }
+    }
+}
