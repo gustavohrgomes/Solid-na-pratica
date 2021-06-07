aa21d7c6bc6a02f0500b60778ddf6276f6adccc9
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-06-07 08:08:14 -0300

    Desacoplando código com interfaces

---
+++ FundamentosPOO/InterfaceDesacopla/IRegistro.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceDesacopla
+{
+    interface IRegistro
+    {
+        void RegistraInfo(string mensagem);
+    }
+}
