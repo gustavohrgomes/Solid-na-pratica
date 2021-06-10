fe379bdf07dc18a1a47993c1e2a9175195b704a9
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-06-10 08:07:30 -0300

    Princípio SRP - Single Responsability Principle

---
+++ PrincipiosSOLID/SRP_Aderente/Pedido.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SRP_Aderente
+{
+    class Pedido
+    {
+        public long Quantidade { get; set; }
+        public DateTime Data { get; set; }
+        private ILogger logger;
+        private EnviarEmail enviarEmail;
+
+        public Pedido()
+        {
+            logger = new RegistraLog();
+            enviarEmail = new EnviarEmail();
+        }
+
+        public void IncluirPedido()
+        {
+            try
+            {
+                logger.Info("Incluindo pedido.");
+                enviarEmail.EmailFrom = "from@email.com";
+                enviarEmail.EmailTo = "to@email.com";
+                enviarEmail.EmailSubject = "Pedido";
+                enviarEmail.EmailBody = "Pedido incluso";
+                enviarEmail.Enviar();
+            }
+            catch (Exception ex)
+            {
+                logger.Info(ex.Message);
+            }
+        }
+        public void DeletaPedido()
+        {
+            try
+            {
+                logger.Info("Removendo pedido.");
+            }
+            catch (Exception ex)
+            {
+                logger.Info(ex.Message);
+            }
+        }
+    }
+}
---
+++ PrincipiosSOLID/SRP_Aderente/RegistraLog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SRP_Aderente
+{
+    class RegistraLog : ILogger
+    {
+        public void Info(string info)
+        {
+            Console.WriteLine(info);
+        }
+    }
+}
