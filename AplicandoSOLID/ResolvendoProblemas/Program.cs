3e81c92f7b1cf991b0315371fef48810a19f814b
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-06-12 10:28:37 -0300

    Resolvendo problemas: Usando Clean Code, switch-case, constantes e enums para melhorar a legibilidade do código

---
+++ AplicandoSOLID/ResolvendoProblemas/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ResolvendoProblemas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GerenciadorDeDesconto gerDesc = new GerenciadorDeDesconto();
+            Console.WriteLine("Valor da compra 1000 e fidelidade 10 anos (5%)\n");
+
+            var resultado = gerDesc.AplicarDesconto(1000, 2, 10);
+            Console.WriteLine($"Cliente tipo 2, 10 anos fidelidade,  = {resultado}");
+
+            var resultado1 = gerDesc.AplicarDesconto(1000, 3, 10);
+            Console.WriteLine($"Cliente tipo 3 o valor do desconto é de : {resultado1}");
+
+            var resultado2 = gerDesc.AplicarDesconto(1000, 4, 10);
+            Console.WriteLine($"Cliente tipo 4 o valor do desconto é de : {resultado2}\n");
+
+            Console.WriteLine("Valor da compra 1000 e fidelidade 4 anos (4%)\n");
+            var resultado3 = gerDesc.AplicarDesconto(1000, 2, 4);
+            Console.WriteLine($"Cliente tipo 2, 10 anos fidelidade,  = {resultado3}");
+
+            var resultado4 = gerDesc.AplicarDesconto(1000, 3, 4);
+            Console.WriteLine($"Para Cliente tipo 3 o valor do desconto é de : {resultado4}");
+
+            var resultado5 = gerDesc.AplicarDesconto(1000, 4, 4);
+            Console.WriteLine($"Para Cliente tipo 4 o valor do desconto é de : {resultado5}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AplicandoSOLID/ResolvendoProblemas/StatusContaCliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResolvendoProblemas
+{
+    public enum StatusContaCliente
+    {
+        NaoRegistrado = 1,
+        ClienteComum = 2,
+        ClienteEspecial = 3,
+        ClienteVip = 4
+    }
+}
