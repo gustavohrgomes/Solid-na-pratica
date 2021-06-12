a301feaa9f8f4448155220d6fa996d0c3cf2419d
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-06-12 12:10:49 -0300

    Testando a implementaçao

--- AplicandoSOLID/ResolvendoProblemas/Program.cs
+++ AplicandoSOLID/ResolvendoProblemas/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ResolvendoProblemas.Enums;
+using ResolvendoProblemas.Interfaces;
+using System;
 
 namespace ResolvendoProblemas
 {
@@ -6,26 +8,30 @@ namespace ResolvendoProblemas
     {
         static void Main(string[] args)
         {
-            GerenciadorDeDesconto gerDesc = new GerenciadorDeDesconto();
+            ICalculaDescontoFidelidade descontoFidelidade = new CalculaDescontoFidelidade();
+            ICalculaDescontoStatusContaFactory descontoStatusConta = new CalculaDescontoStatusContaFactory();
+
+            GerenciadorDeDesconto gerenciadorDesconto = new GerenciadorDeDesconto(descontoFidelidade, descontoStatusConta);
+
             Console.WriteLine("Valor da compra 1000 e fidelidade 10 anos (5%)\n");
 
-            var resultado = gerDesc.AplicarDesconto(1000, 2, 10);
+            var resultado = gerenciadorDesconto.AplicarDesconto(1000, StatusContaCliente.ClienteComum, 10);
             Console.WriteLine($"Cliente tipo 2, 10 anos fidelidade,  = {resultado}");
 
-            var resultado1 = gerDesc.AplicarDesconto(1000, 3, 10);
+            var resultado1 = gerenciadorDesconto.AplicarDesconto(1000, StatusContaCliente.ClienteEspecial, 10);
             Console.WriteLine($"Cliente tipo 3 o valor do desconto é de : {resultado1}");
 
-            var resultado2 = gerDesc.AplicarDesconto(1000, 4, 10);
+            var resultado2 = gerenciadorDesconto.AplicarDesconto(1000, StatusContaCliente.ClienteVip, 10);
             Console.WriteLine($"Cliente tipo 4 o valor do desconto é de : {resultado2}\n");
 
             Console.WriteLine("Valor da compra 1000 e fidelidade 4 anos (4%)\n");
-            var resultado3 = gerDesc.AplicarDesconto(1000, 2, 4);
+            var resultado3 = gerenciadorDesconto.AplicarDesconto(1000, StatusContaCliente.ClienteComum, 4);
             Console.WriteLine($"Cliente tipo 2, 10 anos fidelidade,  = {resultado3}");
 
-            var resultado4 = gerDesc.AplicarDesconto(1000, 3, 4);
+            var resultado4 = gerenciadorDesconto.AplicarDesconto(1000, StatusContaCliente.ClienteEspecial, 4);
             Console.WriteLine($"Para Cliente tipo 3 o valor do desconto é de : {resultado4}");
 
-            var resultado5 = gerDesc.AplicarDesconto(1000, 4, 4);
+            var resultado5 = gerenciadorDesconto.AplicarDesconto(1000, StatusContaCliente.ClienteVip, 4);
             Console.WriteLine($"Para Cliente tipo 4 o valor do desconto é de : {resultado5}");
 
             Console.ReadLine();
