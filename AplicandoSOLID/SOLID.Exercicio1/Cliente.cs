b3d5bfc211493683d1b4f29736cd7817438b5ad0
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-06-14 06:50:58 -0300

    Violacao do principio SRP

---
+++ AplicandoSOLID/SOLID.Exercicio1/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID.Exercicio1
+{
+    class Cliente
+    {
+        public string Nome { get; set; }
+        public string Pais { get; set; }
+        public string Email { get; set; }
+        public static List<Cliente> GetClientes()
+        {
+            List<Cliente> listaClientes = new List<Cliente>()
+            {
+                new Cliente
+                {
+                    Nome="Pedro", Pais="Brasil", Email="pedro@email.com"
+                },
+                new Cliente
+                {
+                    Nome="Maria", Pais="Chile", Email="maria@email.com"
+                }
+            };
+            return listaClientes;
+        }
+    }
+}
